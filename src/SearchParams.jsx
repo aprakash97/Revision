6b571d96efb6d8d49bf503d7bfe1f10f81bf1ae4
Prakash <prakashakrakr@gmail.com>
2023-04-13 20:27:56 +0530

    useState cleared

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit"];
+
 const SearchParams = () => {
   const [location, setLocation] = useState("");
   // const [count, setCount] = useState(0);
@@ -8,6 +10,10 @@ const SearchParams = () => {
   //   event.preventDefault();
   //   setCount(count + 1);
   // }
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = ["adda"];
+
   return (
     <div className="searchParams">
       <form>
@@ -19,6 +25,35 @@ const SearchParams = () => {
             value={location}
             placeholder="Location"
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            {ANIMALS.map((animal) => (
+              <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="animal"
+            value={breed}
+            disabled={breeds.length === 0}
+            onChange={(e) => setBreed(e.target.value)}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed}>{breed}</option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
